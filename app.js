e6908a9bfd48f35122e9ac88803fe1408960940e
hdavella <hdavella@gmail.com>
2023-06-27 15:44:03 -0300

    cambios en las rutas

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const port = 3000
 
 app.use( express.static("public"));
 
+app.get( "/" , (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 app.get( "/generic" , (req, res) => {
   res.sendFile(__dirname + "/public/generic.html");
+});
+
+app.get( "/elements" , (req, res) => {
+  res.sendFile(__dirname + "/public/elements.html");
 });
 
 app.get( "*" , (req, res) => {
